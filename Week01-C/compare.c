bf82ca391b3eb4b20487100ac920830f0c2bbfea
Muhammed Azhar <iam.muhammedazhar@gmail.com>
2024-01-05 01:13:23 +0530

    Fix errors and build 'compare.c' program.

--- Week01-C/compare.c
+++ Week01-C/compare.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
+    int x = 0, y = 0;
+
     printf("What is 'x'? ");
+    scanf("%d", &x);
     printf("What is 'y'? ");
-    int x = scanf("%d", x);
-    int y = scanf("%d", y);
+    scanf("%d", &y);
 
     if(x > y) {
-        printf("'x' is greater than 'y'.");
+        printf("The 'x' (%d) is greater than 'y' (%d).\n", x, y);
+    }
+    else if(x < y) {
+        printf("The 'x' (%d) is less than 'y' (%d).\n", x, y);
+    }
+    else {
+        printf("Both are equal.\n");
     }
 }
