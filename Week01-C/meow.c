00b48be37c161e40101d0aca40f51ce2fe74904a
Muhammed Azhar <iam.muhammedazhar@gmail.com>
2024-01-05 17:50:07 +0530

    Improve code modularity.

--- Week01-C/meow.c
+++ Week01-C/meow.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+void meow(void); // Setting function prototype.
+
 int main(void) {
     for(int i = 0; i < 3; i++) {
-        printf("meow\n");
+        meow();
     }
 
     return 0;
+}
+
+void meow(void) {
+    printf("meow\n");
 }
