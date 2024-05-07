3300ace7bd390a45828cd1b79b784b9ae96eb3f2
Muhammed Azhar <iam.muhammedazhar@gmail.com>
2024-05-08 00:07:26 +0100

    feat: Add pyramid.c program to print a pyramid of '#' characters

---
+++ Week01-C/Section/pyramid.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_rows(int length);
+
+int main(void){
+    int height = get_int("Height: ");
+    for(int i = 0; i < height; i++){
+        print_rows(i + 1);
+    }
+}
+
+void print_rows(int length){
+    for(int i = 0; i < length; i++){
+        printf("#");
+    }
+    printf("\n");
+}
