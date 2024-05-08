73e441c3b7dbf52b9aa977f99bb35b9d918c7be4
Muhammed Azhar <iam.muhammedazhar@gmail.com>
2024-05-08 01:19:04 +0100

    Testing problem set 1.

---
+++ Week01-C/ProblemSet-01/set1.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+    const int n = get_int("Size: ");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            printf(" ");
+        }
+        for(int k = 0; k < i + 1; k++) {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
