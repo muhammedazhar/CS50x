76ab76afe8b24272e93b54c41bd888a3d5b15583
Muhammed Azhar <iam.muhammedazhar@gmail.com>
2024-05-11 03:42:21 +0100

    Update pyramid.c to validate user input for pyramid height

--- Week01-C/Section/pyramid.c
+++ Week01-C/Section/pyramid.c
@@ -1,19 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void print_rows(int length);
+void print_rows(int bricks);
 
 int main(void){
-    const int height = get_int("Height: ");
-    printf("\n");
+    // Prompt the user for the pyramid's height
+    int height;
+    do {
+        height = get_int("Height: ");
+    } while (height < 1);
     for(int i = 0; i < height; i++){
         print_rows(i + 1);
     }
 }
 
-void print_rows(int length){
-    for(int i = 0; i < length; i++){
-        printf("# ");
+void print_rows(int bricks){
+    for(int i = 0; i < bricks; i++){
+        printf("#");
     }
     printf("\n");
 }
